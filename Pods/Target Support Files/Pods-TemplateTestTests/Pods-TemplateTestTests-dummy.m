69d3385a2b567b0a51212834ac95518fdd88b447
Alexey Prokhorov <alexey.prokhorov@shipyardtech.com>
2019-03-11 10:35:48 +0000

    Itinital commit

---
+++ Pods/Target Support Files/Pods-TemplateTestTests/Pods-TemplateTestTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TemplateTestTests : NSObject
+@end
+@implementation PodsDummy_Pods_TemplateTestTests
+@end
---
+++ TemplateTest/Modules/Landing/LandingViewController.swift
@@ -0,0 +1,34 @@
+//
+//  LandingViewController.swift
+//  TemplateTest
+//
+//  Created by Aliaksei Prokharau on 11/03/2019.
+//  Copyright © 2019 Shipyard. All rights reserved.
+//
+
+import UIKit
+
+class LandingViewController: UIViewController {
+
+    @IBOutlet private weak var welcomeLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        welcomeLabel.text = "Hello World!"
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
